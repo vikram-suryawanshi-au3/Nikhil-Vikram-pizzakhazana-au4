0eb86b3a8631c5b183be96f2326d7236b3646b19
vikram <vikramsuryawanshi619.vs@gmail.com>
2020-04-02 23:31:21 +0530

    react app setup

---
+++ pizzakhazana-react-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      react setup is okay...!
+    </div>
+  );
+}
+
+export default App;
